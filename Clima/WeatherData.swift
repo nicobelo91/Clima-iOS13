2ee82a7b379dc608b3b8c233de4f334111882be9
Nico Cobelo <ingcobelo@gmail.com>
2020-11-03 19:39:01 -0500

    Decoded JSON data

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by user183145 on 11/3/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
